0ec88eb8eef216a8db8364b33aaaa50efdd34455
SUBIN PARK <psb8909@naver.com>
2022-11-01 01:54:45 +0900

    ConstraintLayout using the Layout Editor

---
+++ 02.3_ColorMyViews/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.myapplication
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setListeners()
+    }
+
+    private fun makeColored(view: View) {
+
+        when (view.id) {
+
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+
+            R.id.red_button -> binding.boxThreeText.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> binding.boxFourText.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> binding.boxFiveText.setBackgroundResource(R.color.my_green)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+
+    private fun setListeners() {  //클릭리스너 함수
+
+        val boxOneText = binding.boxOneText
+        val boxTwoText = binding.boxTwoText
+        val boxThreeText = binding.boxThreeText
+        val boxFourText = binding.boxFourText
+        val boxFiveText = binding.boxFiveText
+
+        val redButton = binding.redButton
+        val greenButton = binding.greenButton
+        val yellowButton= binding.yellowButton
+
+        val rootConstraintLayout = binding.constraintLayout
+
+        val clickableViews: List<View> =
+            listOf(boxOneText, boxTwoText, boxThreeText,
+                boxFourText, boxFiveText, rootConstraintLayout,
+                redButton, greenButton, yellowButton
+            )
+
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
+    }
+}
